60beaf1363673b0b53940c2e10375891687aa8c3
User <User@DESKTOP-Q9QV65T>
2023-10-18 22:33:19 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, -3, -5, -7, 8, 9 };
+            Console.WriteLine(args.Length);
+            foreach (int j in arr)
+            {
+                Console.WriteLine(j);
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] < 0)
+                    {
+                        arr[i] = -arr[i];
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+
+}
